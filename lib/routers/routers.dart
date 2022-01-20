ba3d8a43d3d768fa85545c5d9f65a6175d6ded85
xiaqingpeng <626143872@qq.com>
2022-01-20 23:10:18 +0800

    路由封装

---
+++ lib/routers/routers.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:kuangxianjiaoapp/view/login_view.dart';
+
+Map<String, WidgetBuilder> routes = {
+  '/login': (BuildContext context) => const LoginView()
+};
---
+++ lib/view/login_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  _LoginViewState createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('登录'),
+      ),
+      body: const Center(
+        child: Text('登录'),
+      ),
+    );
+  }
+}
