7375a02423ba7d2786f543b87cdb36c8d4502420
xiaqingpeng <626143872@qq.com>
2022-05-15 13:41:21 +0800

    增加对日文和韩文支持

--- lib/global/global_international.dart
+++ lib/global/global_international.dart
@@ -1,4 +1,7 @@
+import 'package:get/get_utils/src/extensions/internacionalization.dart';
 List<Map> international = [
-  {"lanuage": "zh", "mark": 'CN', "text": "中文"},
-  {"lanuage": "en", "mark": 'US', "text": "英语"},
+  {"lanuage": "zh", "mark": 'CN', "text": 'chinese'.tr},
+  {"lanuage": "en", "mark": 'US', "text": 'english'.tr},
+  {"lanuage": "ja", "mark": 'JP', "text": 'japanese'.tr},
+  {"lanuage": "ko", "mark": 'KR', "text": 'korean'.tr},
 ];
